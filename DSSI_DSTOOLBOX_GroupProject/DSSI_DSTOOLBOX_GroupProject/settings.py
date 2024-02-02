0e0b5acd8e903544beec78f679582dea077f625b
Kongphop Jamreansuk <kongphop.ja.64@ubu.ac.th>
2024-02-02 22:03:02 +0700

    Update Google Login

--- DSSI_DSTOOLBOX_GroupProject/DSSI_DSTOOLBOX_GroupProject/settings.py
+++ DSSI_DSTOOLBOX_GroupProject/DSSI_DSTOOLBOX_GroupProject/settings.py
@@ -9,6 +9,12 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+import os
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
 
 from pathlib import Path
 
@@ -37,6 +43,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "Googlelogin"
 ]
 
 MIDDLEWARE = [
